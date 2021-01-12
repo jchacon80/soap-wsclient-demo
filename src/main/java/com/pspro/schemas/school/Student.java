a73a62cf717f14ef58ac22bc74d60f83b18f0fdb
jchac <jchac@DESKTOP-OV53H8G>
2021-01-12 17:36:57 +0100

    primera subida

---
+++ src/main/java/com/pspro/schemas/school/Student.java
@@ -0,0 +1,116 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.01.12 a las 05:09:30 PM CET 
+//
+
+
+package com.pspro.schemas.school;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para Student complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="Student"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="standard" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Student", propOrder = {
+    "name",
+    "standard",
+    "address"
+})
+public class Student {
+
+    @XmlElement(required = true)
+    protected String name;
+    protected int standard;
+    @XmlElement(required = true)
+    protected String address;
+
+    /**
+     * Obtiene el valor de la propiedad name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Define el valor de la propiedad name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad standard.
+     * 
+     */
+    public int getStandard() {
+        return standard;
+    }
+
+    /**
+     * Define el valor de la propiedad standard.
+     * 
+     */
+    public void setStandard(int value) {
+        this.standard = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad address.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Define el valor de la propiedad address.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+}
---
+++ src/test/java/com/pspro/soapwsclient/SoapWsclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pspro.soapwsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
